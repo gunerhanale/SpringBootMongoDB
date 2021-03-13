d2fc93562f35e2457ac45de8c420b2f479d96362
gunerhanale <gunerhanale@gmail.com>
2021-03-13 19:06:56 +0100

    wrote Ticket service and impl

---
+++ src/main/java/com/Trackzilla/trackzilla/service/TicketService.java
@@ -0,0 +1,16 @@
+package com.Trackzilla.trackzilla.service;
+
+import com.Trackzilla.trackzilla.data.Release;
+import com.Trackzilla.trackzilla.data.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TicketService {
+
+    public List<Ticket> findAll();
+    Optional<Ticket> findById(String id);
+    Release save(Ticket ticket);
+    void deleteById(String id);
+    void insert(Ticket ticket);
+}
---
+++ src/main/java/com/Trackzilla/trackzilla/service/TicketServiceImpl.java
@@ -0,0 +1,56 @@
+package com.Trackzilla.trackzilla.service;
+
+import com.Trackzilla.trackzilla.data.Release;
+import com.Trackzilla.trackzilla.data.Ticket;
+import com.Trackzilla.trackzilla.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @Override
+    public List<Ticket> findAll() {
+        return ticketRepository.findAll();
+    }
+
+    @Override
+    public Optional<Ticket> findById(String id) {
+        return ticketRepository.findById(id);
+    }
+
+    @Override
+    public Release save(Ticket ticket) {
+        return null;
+    }
+
+    /*@Override
+    public Release save(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }*/
+
+    @Override
+    public void deleteById(String id) {
+        ticketRepository.deleteById(id);
+    }
+
+    public Long countAllTickets(){
+        Stream<Ticket> stream = ticketRepository.findAllByCustomQuery("Open");
+        Long count = stream.count();
+        stream.close();
+        return count;
+    }
+
+    @Override
+    public void insert(Ticket ticket) {
+        ticketRepository.insert(ticket);
+    }
+
+}
