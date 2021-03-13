1d522a2623232a1536dcdf0934e4b2c59202fe67
gunerhanale <gunerhanale@gmail.com>
2021-03-13 16:55:41 +0100

    wrote Application document as a model of mongoDB

--- src/main/java/com/Trackzilla/trackzilla/data/Application.java
+++ src/main/java/com/Trackzilla/trackzilla/data/Application.java
@@ -12,6 +12,42 @@ public class Application {
 
     public Application(){}
 
-    
+    public Application(String id, String name, String description, String owner) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.owner = owner;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
