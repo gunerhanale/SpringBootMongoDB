fbee0e970cdad87e7dbe55af690b2455c9feea19
gunerhanale <gunerhanale@gmail.com>
2021-03-13 17:05:23 +0100

    wrote Release and Ticket documents as a model of mongoDB

--- src/main/java/com/Trackzilla/trackzilla/data/Release.java
+++ src/main/java/com/Trackzilla/trackzilla/data/Release.java
@@ -1,4 +1,57 @@
 package com.Trackzilla.trackzilla.data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
 public class Release {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private List<Ticket> tickets;
+
+    public Release() {
+    }
+
+    public Release(String title, String description, List<Ticket> tickets) {
+        this.title = title;
+        this.description = description;
+        this.tickets = tickets;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
--- src/main/java/com/Trackzilla/trackzilla/data/Ticket.java
+++ src/main/java/com/Trackzilla/trackzilla/data/Ticket.java
@@ -1,4 +1,82 @@
 package com.Trackzilla.trackzilla.data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Ticket {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String appId;
+    private String status;
+
+    public Ticket(String title, String description, String appId, String status) {
+        this.title = title;
+        this.description = description;
+        this.appId = appId;
+        this.status = status;
+    }
+
+    public Ticket(String title, String description, String status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Ticket() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", appId='" + appId + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
