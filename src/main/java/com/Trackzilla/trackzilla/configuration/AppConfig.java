423cc2eca4c89b0695b0843beda3e7842fe5f706
gunerhanale <gunerhanale@gmail.com>
2021-03-15 23:25:41 +0100

    wrote database configurations in AppConfig

--- src/main/java/com/Trackzilla/trackzilla/configuration/AppConfig.java
+++ src/main/java/com/Trackzilla/trackzilla/configuration/AppConfig.java
@@ -1,9 +1,30 @@
 package com.Trackzilla.trackzilla.configuration;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public MongoClient mongo(){
+        ConnectionString connectionString = new ConnectionString("mongodb://localhos:27017/trackzilla");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "trackzilla");
+    }
 
 
 }
